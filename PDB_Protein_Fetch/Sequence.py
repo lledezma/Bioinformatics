28b60a843df45b19f7b3fb28a963867ff79b43b5
lledezma <luis.ledezma@pop.belmont.edu>
2021-12-16 20:29:57 -0600

    clean up, removed redudant code

--- PDB_Protein_Fetch/Sequence.py
+++ PDB_Protein_Fetch/Sequence.py
@@ -3,7 +3,7 @@ import math
 class Test:
 
 	def __init__(self):
-		print("")
+		pass
 
 	def validate(self,sequence):	#QUESTION 1
 		letters = ['A', 'G', 'C', 'T'];
@@ -40,29 +40,29 @@ class Test:
 			return sequence.replace('T','U')
 
 	def translation(self,sequence): #QUESTION 4
-		F = ["UUU", "UUC"] 								#phenylalanie
-		L = ["UUA", "UUG","CUU", "CUC", "CUA", "CUG"] 	#leucine
-		I = ["AUU", "AUC", "AUA"] 						#isoleucine
-		M = ["AUG"]										#Methionine, START
-		V = ["GUU", "GUC", "GUA", "GUG"] 				#valine
-		S = ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"]	#serina
-		P = ["CCU", "CCC", "CCA", "CCG"]				#proline
-		T = ["ACU", "ACC", "ACA", "ACG"]				#Threonine
-		A = ["GCU", "GCC", "GCA", "GCG"]				#Alanine
-		Y = ["UAU", "UAC"]								#Tyrosine
-		STOP = ["UAA", "UAG", "UGA"]								#STOP
-		H = ["CAU", "CAC"]								#histidine
-		Q = ["CAA", "CAG"]								#glutamine
-		N = ["AAU", "AAC"]								#asparagine
-		K = ["AAA", "AAG"]								#lysine
-		D = ["GAU", "GAC"]								#aspartic acid
-		E = ["GAA", "GAG"]								#glutamic acid
-		C = ["UGU", "UGC"]								#cysteine
-		W = ["UGG"]										#tryptophan
-		R = ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"]	#arginine
-		G = ["GGU", "GGC", "GGA", "GGG"]				#glycine
-		codons = [F,L,I,M,V,S,P,T,A,Y,STOP,H,Q,N,K,D,E,C,W,R,G]
-		codonsl = ['F','L','I', 'M','V','S','P','T','A','Y','STOP','H','Q','N','K','D','E','C','W','R','G']
+		codons_dict = [ 
+			("F", ["UUU", "UUC"]), 								#phenylalanie
+			("L" , ["UUA", "UUG","CUU", "CUC", "CUA", "CUG"]), 	#leucine
+			("I ", ["AUU", "AUC", "AUA"]),						#isoleucine
+			("M" , ["AUG"]),									#Methionine, START
+			("V" , ["GUU", "GUC", "GUA", "GUG"]), 				#valine
+			("S" , ["UCU", "UCC", "UCA", "UCG", "AGU", "AGC"]),	#serina
+			("P" , ["CCU", "CCC", "CCA", "CCG"]),				#proline
+			("T" , ["ACU", "ACC", "ACA", "ACG"]),				#Threonine
+			("A" , ["GCU", "GCC", "GCA", "GCG"]),				#Alanine
+			("Y" , ["UAU", "UAC"]),								#Tyrosine
+			("STOP" , ["UAA", "UAG", "UGA"]),					#STOP
+			("H" , ["CAU", "CAC"]),								#histidine
+			("Q" , ["CAA", "CAG"]),								#glutamine
+			("N" , ["AAU", "AAC"]),								#asparagine
+			("K" , ["AAA", "AAG"]),								#lysine
+			("D" , ["GAU", "GAC"]),								#aspartic acid
+			("E" , ["GAA", "GAG"]),								#glutamic acid
+			("C" , ["UGU", "UGC"]),								#cysteine
+			("W" , ["UGG"]),									#tryptophan
+			("R" , ["CGU", "CGC", "CGA", "CGG", "AGA", "AGG"]),	#arginine
+			("G" , ["GGU", "GGC", "GGA", "GGG"]),				#glycine
+		]
 		if(self.transcription(sequence) == False):
 			print("No amino acids")
 			return False
@@ -73,24 +73,20 @@ class Test:
 		n = math.floor(n/3)
 		if tsequence != "":
 			for y in range(n):
-				for j in codons:
+				for j in codons_dict:
 					if (tsequence[(y*3):(y*3+3)]  == "AUG"):
-						if('M' not in protein):
-							protein.append(codonsl[codons.index(M)])
-							break
-						else:
-							protein.append(codonsl[codons.index(M)])
-							break   
+						protein.append('M') 
+						break
 					elif('M' in protein):
-						if j == codons[10]:
-							if ( tsequence[(y*3):(y*3+3)] in j):
+						if j[0] == "STOP":
+							if ( tsequence[(y*3):(y*3+3)] in j[1]):
 								protein = "".join(protein)
 								return protein
-						elif ( tsequence[(y*3):(y*3+3)] in j):
-							protein.append(codonsl[codons.index(j)])
+						elif ( tsequence[(y*3):(y*3+3)] in j[1]):
+							protein.append(j[0])
 							break
 					else:
-						pass
+						continue
 		else:
 			print("No amino acids")
 			return False
@@ -143,5 +139,5 @@ test = Test()
 # print(test.validate("GCAGTCA"))			            				   #QUESTION 1
 # print(test.reverse("GCAGTCA"))                       					   #QUESTION 2
 # print(test.transcription("GCAGTCA"))                  				   #QUESTION 3
-# print(test.translation("AATGGCGCCGATATTATGACGGTCCTTCCTTGATGATAAGGTAA"))  #QUESTION 4
+print(test.translation("AATGGCGCCGATATTATGACGGTCCTTCCTTGATGATAAGGTAA"))  #QUESTION 4
 # test.frames("AATGGCGCCGATATTATGACGGTCCTTCCTTGATGATAAGGTAA")  			   #QUESTION 5
